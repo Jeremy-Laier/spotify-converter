7c30c5667159ba52479147b497802c0615243cd1
jeremy <jeremylaier@outlook.com>
2020-08-08 22:52:51 -0500

    used git ignore :)

--- project/plugins/plugins.scala
+++
@@ -1,3 +0,0 @@
-import sbt.addSbtPlugin
-// In project/plugins.sbt. Note, does not support sbt 0.13, only sbt 1.x.
-addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.0") // "2.4.0" is just sbt plugin version
