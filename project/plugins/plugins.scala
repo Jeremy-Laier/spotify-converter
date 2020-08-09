40a0e45872e488437132e59358982526a547e51d
jeremy <jeremylaier@outlook.com>
2020-08-08 22:33:32 -0500

    adding scala fmt

---
+++ project/plugins/plugins.scala
@@ -0,0 +1,3 @@
+import sbt.addSbtPlugin
+// In project/plugins.sbt. Note, does not support sbt 0.13, only sbt 1.x.
+addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.0") // "2.4.0" is just sbt plugin version
